d8ebaae004097c760f9f0c2c85fd67f4419df6c1
Santiago Pozzolo <pozzolosanti@gmail.com>
2025-04-17 17:36:37 -0300

    Boceto Menu

--- Minashi/src/Main.java
+++ Minashi/src/Main.java
@@ -1,5 +1,32 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String[] opciones = {"Log in", "Registrarse"};
+
+        int seleccion = JOptionPane.showOptionDialog(null, "Bienvenido al Sistema Minashi", "Menu", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
+
+        logIn();
+
     }
+
+
+    static public void logIn(){
+
+
+        String usuarioCorrecto = "Gamaliel";
+        String contrasenaCorrecto = "1234";
+
+        JOptionPane.showInputDialog("Ingrese su usuario");
+        //TODO Validacion si el usuario existe en la base de datos
+
+        JPasswordField password = new JPasswordField();
+
+        JOptionPane.showConfirmDialog(null,password, "Menu", JOptionPane.OK_CANCEL_OPTION);
+        //
+
+
+    }
+
 }
