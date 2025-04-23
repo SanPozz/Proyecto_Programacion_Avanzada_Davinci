d3ad4667c3a56581ee861f8ea40fd33c18a150d7
Sebasonacht <sebasonacht@gmail.com>
2025-04-23 15:45:35 -0300

    Actualizacion: Mostrar funcionalidades en main

--- Minashi/src/Main.java
+++ Minashi/src/Main.java
@@ -26,7 +26,48 @@ public class Main {
         JOptionPane.showConfirmDialog(null,password, "Menu", JOptionPane.OK_CANCEL_OPTION);
         //
 
+<<<<<<< Updated upstream
+=======
+            if (user.equals(usuarioCorrecto) && pass.equals(contrasenaCorrecta)) {
+                JOptionPane.showMessageDialog(null, "Los datos de inicio de sesion son correctos!");
 
+                mostrarFuncionalidades();
+
+
+            } else {
+                JOptionPane.showMessageDialog(null, "Los datos de inicio de sesion son invalidos");
+                reintentar = true;
+            }
+
+        } while(reintentar == true);
     }
+
+    static public void signIn(){
+        JOptionPane.showMessageDialog(null, "Funcionalidad no implementada");
+    }
+
+    static public void mostrarFuncionalidades(){
+
+        String[] opciones = {"Verificar Stock", "Solictar Stock", "Ver Órdenes", "Emitir Órdenes"};
+
+        int seleccion = JOptionPane.showOptionDialog(null, "Que quiere hacer hoy?", "Menu", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
+
+        switch(seleccion){
+            case 0:
+                JOptionPane.showMessageDialog(null, "Minerales en stock");
+                break;
+            case 1:
+                JOptionPane.showMessageDialog(null, "Solicitar Minerales");
+                break;
+            case 2:
+                JOptionPane.showMessageDialog(null, "Órdenes en curso");
+                break;
+            case 3:
+                JOptionPane.showMessageDialog(null, "Finalizar proceso de venta");
+                break;
+            default:
+                break;
+        }
+>>>>>>> Stashed changes
 
 }
